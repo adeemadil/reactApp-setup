916a90bf17c24a9111be4671baa04a7d9289683c
Adeem Adil Khatri <adil.khatri@ug.bilkent.edu.tr>
2022-09-01 18:16:16 +0500

    edit: new changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 8, 1);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.5;
+function ExpenseItem(props) {
+  
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__descsription">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
