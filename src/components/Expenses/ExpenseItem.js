8a66f5ec20ffcb4416cb6460e62982fcb6424a04
Adeem Adil Khatri <adil.khatri@ug.bilkent.edu.tr>
2022-09-13 19:19:08 +0500

    edit: dynamic items

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,19 @@
-import React, {useState} from 'react';
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
-  const [title, setTitle] = useState(props.title)
-
-  const clickHandler = () => {
-    setTitle('Updated!')
-    console.log(title)
-  }
-
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__descsription">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
